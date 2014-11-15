8357ebd78665b4387940826fb0b3fe74d42243d5
aWakeParks <p.pugstaller@gmail.com>
2014-11-15 18:38:41 +0100

    Go Tigers!

--- lists/tests.py
+++ lists/tests.py
@@ -81,3 +81,24 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class NewListTest(TestCase):
+
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertEqual(Item.object.count(), 1)
+        new_item = Item.object.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'],
+                         '/lists/the-only-list-in-the-world/')
