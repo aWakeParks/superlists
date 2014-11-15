d7e381ef7d8cbe529acc362b9feb4a5017703396
aWakeParks <p.pugstaller@gmail.com>
2014-11-15 18:31:10 +0100

    Go Tigers!

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -48,7 +48,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
-        #(Edith is very methodical)
+        # (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
@@ -74,8 +74,15 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
         # Francis gets his own unique URl
         francis_list_url = self.browser.current_url
+        import time
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
